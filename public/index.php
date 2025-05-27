de471c8631e84431c3902c947af826dcf36480d2
Yan <yan2005leal@gmail.com>
2025-05-27 20:59:35 -0300

    Alterações finais

--- public/index.php
+++ public/index.php
@@ -1,12 +1,13 @@
 <?php 
 
+    session_start();
+
     require __DIR__ . '/../app/config/config.php';
     require AUTOLOAD;
     
     Routes::defineRoutes();
 
-    if (session_status() != 1) {
-        session_start();
-    }
+    
+
 ?>
         
